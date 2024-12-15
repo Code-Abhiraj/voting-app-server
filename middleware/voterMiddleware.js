b9c317138983444de5b4b8baa01000950590c3fc
Code-Abhiraj <111672801+Code-Abhiraj@users.noreply.github.com>
2024-12-15 13:04:12 +0530

    Update voterMiddleware.js

--- middleware/voterMiddleware.js
+++ middleware/voterMiddleware.js
@@ -5,20 +5,26 @@ const VoterMiddleware = (req, res, next) => {
         const token = req.headers.token;
         if(!token) {
             return res.status(406).json({
-                'message': 'Token not found'
+                message: 'Token not found'
             });
         }
         const decoded = jwt.verify(token, process.env.VOTER_SECRET);
         if(!decoded) {
             return res.status(407).json({
-                'message': 'Invalid Token'
+                message: 'Invalid Token'
+            });
+        }
+        if(decoded.role != 'voter')
+        {
+            return res.status(406).json({
+                message: 'Only voters are allowed'
             });
         }
         req.voterId = decoded.id;
         next();
     } catch(err) {
         return res.status(500).json({
-            'message' : err
+            message : err
         });
     }
 }
